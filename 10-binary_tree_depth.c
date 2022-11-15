c7fb0183e04ee9bc5e387a0ddc98f0dde8fbf95f
erickadikah <erickadikah2030@gmail.com>
2022-11-15 12:56:03 -0800

    task 10

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * binary_tree_depth: l_Depth
+ * binary_tree_depth - l_Depth
+ * @tree: pointer to the tree
  * Return: l_Depth
  */
 
@@ -11,10 +12,9 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
-	else
-	{
-		size_t l_Depth = 0;
-		l_Depth = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
-		return (l_Depth);
-	}
+
+	size_t l_Depth = 0;
+
+	l_Depth = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
+	return (l_Depth);
 }
