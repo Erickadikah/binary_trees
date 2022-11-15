40ac94ddd05b61761aba30bf5535d942dff2f6f5
erickadikah <erickadikah2030@gmail.com>
2022-11-15 12:46:21 -0800

    new

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -14,7 +14,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 	else
 	{
 		size_t l_Depth = 0;
-		l_Depth = tree->prent ? 1 + binary_tree_depth(tree->parent) : 0;
+		l_Depth = tree->parent ? 1 + binary_tree_depth(tree->parent) : 0;
 		return (l_Depth);
 	}
 }
