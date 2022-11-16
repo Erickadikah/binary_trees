7154e112ac5aebdac022043ee76329be09094185
IamLam163 <olamidealiyu@gmail.com>
2022-11-16 01:59:49 -0800

    print

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,26 +1,22 @@
 #include "binary_trees.h"
-#include <stdlib.h>
+
 /**
- * binary_tree_insert_right - is the binary tree
- * @parent: is the pointer to the root node
- * @value: this is the value a tree holds
- * Return: if an error occures - NULL.
+ * binary_tree_insert_right- insert to the right of a node
+ * @parent: parent node to insert
+ * @value: value of the new node
+ * Return: return address of new node
  */
+
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-
 	binary_tree_t *new, *temp;
 
 	new = malloc(sizeof(binary_tree_t));
-
-	/* parent is a pointer to the parent node of the node to create */
 	if (new == NULL)
 		return (NULL);
-
 	new->left = NULL;
 	new->right = NULL;
 	new->n = value;
-
 	if (parent->right == NULL)
 	{
 		parent->right = new;
