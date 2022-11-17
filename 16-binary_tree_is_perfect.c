74eda5f4e01b310a5aa775b134520007b1da96c4
IamLam163 <olamidealiyu@gmail.com>
2022-11-17 02:35:30 -0800

    tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,75 +1,70 @@
 #include "binary_trees.h"
 /**
- * binary_tree_leaves - counts the number of leaves
- * @tree: pointer to root node
- * Return: returns the number of leaves
+ * binary_tree_size - size of the tree
+ * @tree: pointer to the node
  */
 
-
-size_t binary_tree_leaves(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
-	{
-		size_t l_leaf = 0, r_leaf = 0;
-
-		if ((tree->left == NULL) && (tree->right == NULL))
-			return (1);
-		l_leaf = binary_tree_leaves(tree->left);
-		r_leaf = binary_tree_leaves(tree->right);
-		return (l_leaf + r_leaf);
-	}
+    if (tree == NULL)
+        return (0);
+    else
+    {
+        size_t l_size = 0, r_size = 0;
+        l_size = tree->left ? binary_tree_size(tree->left) : 0;
+        r_size = tree->right ? binary_tree_size(tree->right) : 0;
+        return (l_size + r_size + 1);
+    }
 }
 
-
 /**
- * binary_tree_height - computes the height of binary tree
- * @tree: pointer to the root node
- * Return: return height of the tree
+ * binary_tree_height - the height of the tree
+ * @tree: pointer to the node
  */
-
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	if (tree)
-	{
-		size_t l = 0, r = 0;
-
-	if (tree->left)
-	{
-		l = 1 + binary_tree_height(tree->left);
-	}
-	else
-		l = 0;
-
-	r = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-	return ((l > r) ? l : r);
-	}
-	return (0);
-}
-
-/**
- * binary_tree_is_perfect - computes if the binary tree is perfect
- * @tree: pointer to the root node
- * Return: returns 1 if the tree is perfect otherwise 0
- */
-
-
-int binary_tree_is_perfect(const binary_tree_t *tree)
-{
-	binary_tree_t *l, *r;
-
 	if (tree == NULL)
 		return (0);
-	l = tree->left;
-	r = tree->right;
-	if (binary_tree_is_leaf(tree))
-		return (1);
-	if (l == NULL || r == NULL)
-		return (0);
-			if (binary_tree_height(l) == binary_tree_height(r))
-			{
-				if (binary_tree_is_perfect(l) && binary_tree_is_perfect(r))
-					return (1);
-			}
+	else
+	{
+		size_t l_Height = 0, r_Height = 0;
+		l_Height = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+		r_Height = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+		if (l_Height >= r_Height)
+			return (l_Height);
+		else
+			return (r_Height);
+	}
+}
+int power(int base, int exp)
+{
+	int power = 1, i = 1;
+
+	for (i = 1; i <= exp; i++)
+	{
+		power = power * base;
+	}
+	return (power);
+}
+/**
+ * binary_tree_is_perfect - function checks if tree is perfect
+ * @tree: pointer to the node
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree)
+{
+	int size = 0, height = 0, nodes = 0;
+
+	if (tree)
+	{
+
+		height = binary_tree_height(tree) + 1;
+		size = binary_tree_size(tree);
+		nodes = power(2, height);
+
+		if (size == (nodes - 1))
+			return (1);
+		else
+			return (0);
+	}
 	return (0);
 }
